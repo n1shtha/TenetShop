d5e88477b3798ef618e6b6c7bd1c790f88568325
shalinibalodi <shalini.balodi_ug23@ashoka.edu.in>
2021-11-20 00:18:09 +0530

    Solved warnings

--- frontend/src/component/Home/Home.js
+++ frontend/src/component/Home/Home.js
@@ -10,7 +10,7 @@ import { useAlert } from "react-alert";
 const Home = () => {
   const alert = useAlert();
   const dispatch = useDispatch();
-  const { loading, error, products, productsCount } = useSelector(
+  const { loading, error, products } = useSelector(
     (state) => state.products
   );
 
